90add35fbedf5178235fd6f4c091b9426c54fa1a
arrxssl <165049807+arrxssl@users.noreply.github.com>
2024-09-12 12:13:04 +1000

    Add files via upload

---
+++ dsfdsf/script.js
@@ -0,0 +1,39 @@
+//let name = "Данил";
+//alert( `Привет, ${name}!`);
+//alert (`результат: ${1 +2}`);
+
+//let s = "моя" + "строка";
+//alert(s);
+
+//let hour=9;
+//if(hour<10||hour>18){
+//    alert(`Офис закрыт.`);
+//}
+
+//let a = 2 * (3-1);
+//let b = 6-2;
+//alert (a==b)
+
+//let a=5 * (7-4);
+//let b=1+2+7;
+//alert(a>b)
+
+//let a=2**4;
+//let b=4**2;
+//alert(a != b)
+
+//const user={
+//    name:"Jacob",
+//age:30
+//};
+//user.age=44;
+//alert(user.age);
+
+// let user ={
+//     name:"Jacob",
+//     age:30
+// };
+// let key = prompt("Что вы хотите узнать о порльзователе?");
+
+// alert( user[key] );
+
